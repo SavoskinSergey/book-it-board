e9d81e885768f4640dd4a5534a05e50882af181f
Sergey Savoskin <savoskin_yandex@mail.ru>
2023-05-09 12:44:04 +0300

    add signal to create mail for EventCreate

--- core/event/signals.py
+++ core/event/signals.py
@@ -4,6 +4,7 @@ from django.core.mail import send_mail
 from django.dispatch import receiver
 
 from core.account.models import User
+from core.mail.models import Mail
 from .models import Event
 
 
@@ -22,3 +23,26 @@ def mail(sender, instance, created, **kwargs):
             from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=[recipient.email],
         )
+
+
+@receiver(post_save, sender=Event)
+def mail_mark(sender, instance, created, **kwargs):
+    if created:
+        recipient = User.objects.get_object_by_public_id(
+                instance.admin.public_id
+                )
+        message = f'Пользователь {recipient.username} добавил событие '
+        f'{instance.body} на дату '
+        f'{instance.event_data}'
+        Mail.objects.create(
+            subject='Create Event',
+            body=message,
+            sender_name=settings.DEFAULT_FROM_EMAIL,
+            sender_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_name=recipient.username,
+            recipient_email=recipient.email,
+            recipient_phone=recipient.phone_number,
+            # event_status = "",
+            status_send=True,
+            send_by_mail=True
+            )
